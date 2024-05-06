d518aec2a8d0252f71ec5f65328e57d7cc6a9430
malliste <malliste@student.21-school.ru>
2024-05-06 18:00:17 +0300

    main

--- build/CMakeFiles/cube_installer_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/cube_installer_autogen_timestamp_deps"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cube_installer_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
